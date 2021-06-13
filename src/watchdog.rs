85c2c8af402090ebd68242b723492c2841a56d2d
Kshitij <kshitijk20@iitk.ac.in>
2021-06-13 18:44:41 +0530

    #1; completed the fn disable function

--- src/watchdog.rs
+++ src/watchdog.rs
@@ -29,8 +29,11 @@ impl Watchdog {
             core::ptr::write_volatile(&mut self.unlock, 0xD928);
             __nop(); // 2 cycle delay for every unlock
             __nop();
+            let mut ctrl = core::ptr::read_volatile(&self.stctrlh);// reading into the Watdog Status and Control Register High
+            ctrl &= !(0x00000001); // changing the 0th bit 
+            core::ptr::write_volatile(&mut self.stctrlh, ctrl);// writing into the register
+            
 
-            let mut ctrl = core::ptr::read_volatile(&self.stctrlh);
             // Disable the watchdog. This has 2 parts, unlocking the watchdog for modification and then disabling the watchdog.
             // See section 23.3.1 for unlocking the watchdog. Ignore point 3 there.
             // To disable the watchdog, see section 23.7.1 and scroll down to the last item in the table the 0th bit to understand how to disable the watchdog. This makes it clear that your operation should only change the 0th bit in the 16-bit value, keeping others same. How would you do that? (Think XOR,AND,OR etc.)
