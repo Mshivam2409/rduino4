765788082b41bdfa341cbf90219b240d02381540
Richa <richa20@iitk.ac.in>
2021-06-18 20:07:12 +0530

    timer disabled+clock gate on+modified pin to GPIO

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,22 @@ mod port;
 mod sim;
 mod watchdog;
 
+use port::Port;
+use sim::Sim;
 use watchdog::Watchdog;
 
 pub extern "C" fn main() {
     // Disable watchdog timer
-    unsafe { Watchdog::new() }.disable();
+    //let(dogw,sim,pin)= unsafe { (Watchdog::new(),Sim::new(),) }.disable();
+    let dogw = unsafe { Watchdog::new() };
+    let sim = unsafe { Sim::new() };
+    let pin = unsafe { Port::new(port::PortName::C).pin(5) };
+
+    dogw.disable();
+    sim.enable_clock(sim::Clock::PortC);
+    let mut gpio = pin.make_gpio();
+    gpio.output();
+    gpio.high();
 
     loop {}
 }
@@ -32,7 +43,7 @@ pub static _VECTORS: [unsafe extern "C" fn(); 2] = [_stack_top, main];
 // Complete the code below
 
 pub static _FLASHCONFIG: [u8; 16] = [
-    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xDE, 0xF9, 0xFF, 0xFF, 
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xDE, 0xF9, 0xFF, 0xFF,
 ];
 
 #[panic_handler]
