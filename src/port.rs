80b0f142a7b1abec7c171bef9639a293d856e44e
Aniket <sharmaa20@iitk.ac.in>
2021-06-26 22:19:31 +0530

    Done port.rs but others erased due to some reasons soon will do them

--- src/port.rs
+++ src/port.rs
@@ -1,55 +1,53 @@
 use core;
 
-#[derive(Clone, Copy)]
+#[derive(Clone,Copy)]
 pub enum PortName {
-    C,
+    C
 }
 
-#[repr(C, packed)]
-pub struct Port {
-    // Complete the struct below. See section 11.1.4 of the manual. Note it has continous memory representation of multiple ports but struct should only account for port C i.e. all registers beginning with PORTC_.
-    pcr: [u32; 32],
-    gpclr: u32,
-    gpchr: u32,
-    reserved_0: [u8; 24],
-    isfr: u32,
+#[repr(C,packed)]
+pub struct Port { //registers in port c
+    pcr : [u32;32], //32 Pin Control Register n
+    gpclr : u32, // Global Pin Control Low Register (PORTC_GPCLR)
+    gpchr : u32, // Global Pin Control High Register (PORTC_GPCHR) 
+    isfr : u32, // Interrupt Status Flag Register(PORTC_ISFR)
 }
 
 impl Port {
     pub unsafe fn new(name: PortName) -> &'static mut Port {
-        // Complete the function below. Similar to watchdog. But use a matchcase since we should only return when portname is C. See the address in section 11.1.4.
         &mut *match name {
-            PortName::C => 0x4004B000 as *mut Port,
+            PortName::C => 0x4004B000 as *mut Port,// return address only if the port is c
         }
     }
-
     pub unsafe fn set_pin_mode(&mut self, p: usize, mut mode: u32) {
-        // Given the pin mode as a 32 bit value set the register bytes to the same value for the corresponding pin. See the MUX(10-8) bits in section 11.14.1. We need to set only those bits. Again think of appropriate operations using AND,OR,XOR etc.. There are only 8 possible pin models so mode = 0 to 7. Reject if different.
-        let mut pcr = core::ptr::read_volatile(&self.pcr[p]);
-        pcr &= 0xFFFFF8FF;
-        mode &= 0x00000007;
-        mode <<= 8;
-        pcr |= mode;
-        core::ptr::write_volatile(&mut self.pcr[p], pcr);
+        let mut pcr= core::ptr::read_volatile(&self.pcr[p]);
+
+        pcr &= 0xFFFFF8FF;// to make the value at 8-10 to be 0 
+        mode &= 0x00000007;//to make all other value except 0-2 in mode to zero
+        mode <<= 8;//shift mode's value to left by 8
+        pcr |= mode;//take or to get the value of mode in pcr  
+
+        core::ptr::write_volatile(&mut self.pcr[p],pcr);
     }
 }
 
 pub struct Pin {
     port: *mut Port,
-    pin: usize,
+    pin: usize
 }
 
 impl Port {
     pub unsafe fn pin(&mut self, p: usize) -> Pin {
-        // Complete and return a pin struct
-        Pin { port: self, pin: p }
+        Pin{
+            port : self,
+            pin : p
+        }
     }
 }
 
-#[repr(C, packed)]
+#[repr(C,packed)]
 struct GpioBitBand {
-    // Complete using section 49.2
-    pdor: u32,
+    pdor: u32,// gpio registers for Port C
     psor: u32,
     pcor: u32,
     ptor: u32,
@@ -58,17 +56,16 @@ struct GpioBitBand {
 }
 
 pub struct Gpio {
-    gpio: *mut GpioBitBand,
-    pin: usize,
+    gpio: *mut GpioBitband,
+    pin: usize
 }
 
 impl Port {
     pub fn name(&self) -> PortName {
         let addr = (self as *const Port) as u32;
         match addr {
-            // Return PortName::C if the address matches the starting address of port C as specified in section 11.1.4. Reject if address is wrong and return error.
-            0x4004B000 => PortName::C,
-            _ => unreachable!(),
+            0x4004B000 => PortName::C,//if address matches that of Port C
+            _ => unreachable!(),//if address does not match that of Port C
         }
     }
 }
@@ -76,19 +73,18 @@ impl Port {
 impl Pin {
     pub fn make_gpio(self) -> Gpio {
         unsafe {
-            // Set pin mode to 1 to enable gpio mode (section 11.14.1 MUX bits).
             // Consume the pin into a gpio struct i.e. instantitate a gpio struct using the new function below.
-            let port = &mut *self.port;
-            port.set_pin_mode(self.pin, 1);
-            Gpio::new(port.name(), self.pin)
+            let port: &mut Port=&mut*self.port;//make port to be taken as mutable reference of Port
+            port.set_pin_mode(self.pin, 1);// to make a pin into gpio mode 
+            Gpio::new(port.name(), self.pin)//making the new Gpio struct for the port given and pin given 
         }
     }
 }
 
 impl Gpio {
-    pub unsafe fn new(port: PortName, pin: usize) -> Gpio {
+    pub unsafe fn new(port: PortName, pin: usize) -> Gpio {-++++
         let gpio = match port {
-            PortName::C => 0x43FE1000 as *mut GpioBitBand,
+            PortName::C => 0x43FE1000 as *mut GpioBitband
         };
 
         // Initialize and return a gpio struct.
@@ -97,17 +93,15 @@ impl Gpio {
 
     pub fn output(&mut self) {
         unsafe {
-            //  WRITE THE  XX register of GPIO to 1 to enable this pin as output type.
-            // See section 49.2 of the teensy manual to find out what is XX.
-            core::ptr::write_volatile(&mut (*self.gpio).pddr, 1);
+            //  WRITTEN Port Data Direction Register register of GPIO to 1 to enable this pin as output type.
+            core::ptr::write_volatile(&mut (*self.gpio).pddr,1);
         }
     }
 
     pub fn high(&mut self) {
         unsafe {
-            //  WRITE THE  XX register of GPIO to 1 to set this pin as high.
-            // See section 49.2 of the teensy manual to find out what is XX. Please not that it is not PDOR, since PDOR is never directly written.
-            core::ptr::write_volatile(&mut (*self.gpio).psor, 1);
+           //  WRITTEN Port Set Output Register register of GPIO to 1 to set this pin as high.
+           core::ptr::write_volatile(&mut (*self.gpio).psor, 1);
         }
     }
 }
