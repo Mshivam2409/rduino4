548bdab291bad44ef6c9b49d487dc7d9b4e7c462
Aniket <sharmaa20@iitk.ac.in>
2021-06-26 23:02:13 +0530

    #5 Updating Port.rs with more comments

--- src/port.rs
+++ src/port.rs
@@ -6,7 +6,8 @@ pub enum PortName {
 }
 
 #[repr(C,packed)]
-pub struct Port { //registers in port c
+pub struct Port { // see section 49.2 in Teensy manual for help
+    //registers in port c
     pcr : [u32;32], //32 Pin Control Register n
     gpclr : u32, // Global Pin Control Low Register (PORTC_GPCLR)
     gpchr : u32, // Global Pin Control High Register (PORTC_GPCHR) 
@@ -20,10 +21,9 @@ impl Port {
         }
     }
     pub unsafe fn set_pin_mode(&mut self, p: usize, mut mode: u32) {
-        let mut pcr= core::ptr::read_volatile(&self.pcr[p]);
-
+        let mut pcr= core::ptr::read_volatile(&self.pcr[p]);//Reading value of the pcr of the Pin in Gpio to set pin mode
         pcr &= 0xFFFFF8FF;// to make the value at 8-10 to be 0 
-        mode &= 0x00000007;//to make all other value except 0-2 in mode to zero
+        mode &= 0x00000007;//to make all other value except 0-2 in mode to be zero and values in 0-2 to be 1
         mode <<= 8;//shift mode's value to left by 8
         pcr |= mode;//take or to get the value of mode in pcr  
 
@@ -31,14 +31,14 @@ impl Port {
     }
 }
 
-pub struct Pin {
-    port: *mut Port,
-    pin: usize
+pub struct Pin { // Pin is not stored as a register in port but as a struct in Port 
+    port: *mut Port, //  Reference to Port of which we are using the pin
+    pin: usize // Pin number to be used
 }
 
 impl Port {
     pub unsafe fn pin(&mut self, p: usize) -> Pin {
-        Pin{
+        Pin{ // assigns Pin and Port to the struct Pin
             port : self,
             pin : p
         }
@@ -55,14 +55,14 @@ struct GpioBitBand {
     pddr: u32,
 }
 
-pub struct Gpio {
+pub struct Gpio { // Gpio is also stored as a struct
     gpio: *mut GpioBitband,
     pin: usize
 }
 
 impl Port {
     pub fn name(&self) -> PortName {
-        let addr = (self as *const Port) as u32;
+        let addr = (self as *const Port) as u32;//store address of port passed in addr
         match addr {
             0x4004B000 => PortName::C,//if address matches that of Port C
             _ => unreachable!(),//if address does not match that of Port C
@@ -76,7 +76,7 @@ impl Pin {
             // Consume the pin into a gpio struct i.e. instantitate a gpio struct using the new function below.
             let port: &mut Port=&mut*self.port;//make port to be taken as mutable reference of Port
             port.set_pin_mode(self.pin, 1);// to make a pin into gpio mode 
-            Gpio::new(port.name(), self.pin)//making the new Gpio struct for the port given and pin given 
+            Gpio::new(port.name(), self.pin)//making the Gpio struct for the port given and pin given 
         }
     }
 }
@@ -84,7 +84,7 @@ impl Pin {
 impl Gpio {
     pub unsafe fn new(port: PortName, pin: usize) -> Gpio {-++++
         let gpio = match port {
-            PortName::C => 0x43FE1000 as *mut GpioBitband
+            PortName::C => 0x43FE1000 as *mut GpioBitband// starting address for the registers in the Gpio
         };
 
         // Initialize and return a gpio struct.
